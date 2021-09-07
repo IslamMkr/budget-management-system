4920252360ba31b021d5b07e6e2d10bcf68add2b
Islam Mokrane <islammkr01@gmail.com>
2021-09-08 00:41:22 +0100

    Added scrollbar style.

--- dist/react.js
+++ dist/react.js
@@ -32003,8 +32003,8 @@ var formatNumberToCurrency = function (number) {
         style: 'currency',
         currency: 'DIN',
         // These options are needed to round to whole numbers if that's what you want.
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 3, // (causes 2500.99 to be printed as $2,501)
+        //minimumFractionDigits: 2, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 3, // (causes 2500.99 to be printed as $2,501)
     });
     return formatter.format(number);
 };
